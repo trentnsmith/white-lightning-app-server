8af985c2cb23420fe1eae17bfa8ac5de30560277
Trent Smith <trentnsmith@gmail.com>
2020-07-19 21:46:02 -0500

    import distillery router

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
-require('dotenv').config()
-const express = require('express')
-const morgan = require('morgan')
-const cors = require('cors')
-const helmet = require('helmet')
-const { NODE_ENV } = require('./config')
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
+const { NODE_ENV } = require('./config');
+const distilleryRouter = require('./distilleries/distilleries-routers');
 
 const app = express()
 
@@ -15,6 +16,8 @@ const morganOption = (NODE_ENV === 'production')
 app.use(morgan(morganOption))
 app.use(helmet())
 app.use(cors())
+
+app.use('/api/distilleries', distilleryRouter)
 
 app.get('/', (req, res) => {
     res.send('Hello, world!')
