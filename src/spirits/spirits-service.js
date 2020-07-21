fca38f9b59cf853b359b3a95fd2fd33b61dbc02e
Trent Smith <trentnsmith@gmail.com>
2020-07-20 23:12:43 -0500

    createSpirit working

--- src/spirits/spirits-service.js
+++ src/spirits/spirits-service.js
@@ -3,6 +3,16 @@ const SpiritsService = {
         return db
             .select('*')
             .from('white_lightning_spirits');
+    },
+
+    createSpirit(db, spirit) {
+        return db
+            .insert(spirit)
+            .into('white_lightning_spirits')
+            .returning('*')
+            .then((rows) => {
+                return rows[0]
+            });
     }
 };
 
