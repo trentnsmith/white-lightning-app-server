6f9ee6444cd01c0301736acaa624c48757c5c760
Trent Smith <trentnsmith@gmail.com>
2020-07-20 23:10:29 -0500

    post endpoint creating, working

--- src/spirits/spirits-router.js
+++ src/spirits/spirits-router.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const SpiritsService = require('./spirits-service');
 const spiritsRouter = express.Router();
 const jsonParser = express.json();
@@ -21,6 +22,30 @@ spiritsRouter
                 res.json(spirit.map(serializeSpirit))
             })
             .catch(next)
+    })
+    .post(jsonParser, (req, res, next) => {
+        const { spirit_name, content, age, abv, category, distillery_id } = req.body;
+        const newSpirit = { spirit_name, content, age, abv, category, distillery_id };
+
+        for (const [key, value] of Object.entries(newSpirit)) {
+            if (!value) {
+                return res.status(400).json({
+                    error: { message: `Missing '${key}' in request body`}
+                });
+            };
+        };
+
+        SpiritsService.createSpirit(
+            req.app.get('db'),
+            newSpirit
+        )
+        .then(spirit => {
+            res
+                .status(201)
+                .location(path.posix.join(req.originalUrl, `/${spirit.id}`))
+                .json(serializeSpirit(spirit))
+        })
+        .catch(next)
     })
 
 module.exports = spiritsRouter;
