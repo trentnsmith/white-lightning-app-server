730575fb4d5837584712b998f3d0b3110759396a
Trent Smith <trentnsmith@gmail.com>
2020-07-20 22:46:33 -0500

    working on test cases for get distilleries endpoint

---
+++ test/distilleries-endpoints.spec.js
@@ -0,0 +1,47 @@
+const knex = require('knex');
+const app = require('../src/app');
+const makeDistilleryArray = require('./distilleries-fixtures');
+const supertest = require('supertest');
+const { expect } = require('chai');
+
+describe('Distilleries endpoints', () => {
+    let db;
+
+    before('make knex instance', () => {
+        db = knex({
+            client: 'pg',
+            connection: process.env.TEST_DATABASE_URL,
+        });
+        app.set('db', db)
+    });
+
+
+
+
+    describe('GET /api/distilleries', () => {
+        context('Given no courses', () => {
+            it('responds with 200 and an empty list', () => {
+                return supertest(app)
+                    .get('/api/distilleries')
+                    .expect(200)
+            });
+        });
+
+        context('Given there are distilleries in the database', () => {
+            const testDistilleries = makeDistilleryArray();
+            
+            beforeEach('insert distilleries', () => {
+                return db 
+                    .into('white_lightning')
+                    .insert(testDistilleries)
+            });
+
+            it('responds with 200 and all of the courses', () => {
+                return supertest(app)
+                    .get('/api/distilleries')
+                    .expect(200)
+            });
+        });
+    });
+});
+
---
+++ test/distilleries-fixtures.js
@@ -0,0 +1,24 @@
+function makeDistilleryArray() {
+    return [
+        {
+           
+            "name": "Brickway Brewery and Distillery",
+            "description": "Located in the heart of Omaha’s Old Market District, Brickway Brewery & Distillery taps into Omaha’s rich history by handcrafting beer and spirits that are as tried-and-true as the brick paved streets",
+            "website": "www.drinkbrickway.com"
+        },
+        {
+           
+           "name": "Cut Spike Distillery",
+           "description": "Cut Spike is a nod to Omaha's railroad history with pride of doing the work by hand. An Artisan distillery located in La Vista, NE specializing in distilling premium spirits.",
+           "website": "www.cutspikedistillery.com" 
+        },
+        {
+           
+            'name': "Patriarch Disillers Inc",
+            "description": "Patriarch Distillers originated from a desire to celebrate and honor U.S. veterans. The name embodies the vision of the company's commitment to family with the term 'Patriarch' meaning 'head of family'.",
+            "website": "www.soldiervalleyspirits.com"
+        }
+    ];
+};
+
+module.exports = makeDistilleryArray;
