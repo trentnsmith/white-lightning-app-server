758cac52807575b30b2c10e75522b54d18f5cb98
Trent Smith <trentnsmith@gmail.com>
2020-07-20 21:54:36 -0500

    adding seed files

---
+++ seeds/seed.white_lightning_distilleries.sql
@@ -0,0 +1,5 @@
+INSERT INTO white_lightning_distilleries (distillery_name, website, description)
+VALUES
+    ('Brickway Brewery and Distillery', 'www.drinkbrickway.com', 'Located in the heart of Omaha’s Old Market District, Brickway Brewery & Distillery taps into Omaha’s rich history by handcrafting beer and spirits that are as tried-and-true as the brick paved streets'),
+    ('Cut Spike Distillery', 'www.cutspikedistillery.com', 'Cut Spike is a nod to Omaha railroad history with pride of doing the work by hand. An Artisan distillery located in La Vista, NE specializing in distilling premium spirits'),
+    ('Patriarch Disillers Inc', 'www.soldiervalleyspirits.com', "Patriarch Distillers originated from a desire to celebrate and honor U.S. veterans. The name embodies the vision of the company's commitment to family with the term 'Patriarch' meaning 'head of family'");
---
+++ seeds/seed.white_lightning_spirits.sql
@@ -0,0 +1,20 @@
+
+INSERT INTO white_lightning_spirits(spirit_name, category, distillery_id, age, abv, content)
+VALUES
+ ('Honest American', 'Single Malt Whisky', 1, '', '', "You don't arrive at a whisky this smooth, this captivating by accident. This whisky is a single malt, double pot distilled like the masters of Scotland, then aged in new American charred oak barrels in the bourbon tradition."),
+ ('Honest American', 'White Whisky', 1, '7 days', '', '"Straight up, un aged goodness! Aged for 7 days on new oak, this 100% malted barley lends an assertive grain flavor with firm sweetness."'),
+ ('Honest American', 'Gin', 1, '', '', 'This is a gin with some tase of gin?'),
+ ('Koenig Dead Devil', 'Cinnamon Whisky', 1, '7 days', '', '"This whisky is derived from the White Whisky by steeping cinnamon shavings for nearly one week. It is all natural, no gross flavors or additives were used to make this tasty favorite."'),
+ ('Mostly Honest', 'Bourbon', 1, '', '', '"Aged in American charred oak barrels, this bourbon is taste tested until it is perfect."'),
+ ('Mostly Honest', 'Vodka', 1, '', '', '"Made from a base of corn and barley, this vodka does what it is supposed to, and it is odorless and tasteless."'),
+ ('Libertas Colonial', 'Rum', 1, '', '', 'This is a rum that taste like rum'),
+ ('Cut Spike Whiskey', 'Whiskey', 2, '', '', 'This is a whiskey that tastes like whiskey'),
+ ('Cut Spike Vodka', 'Vodka', 2, '', '', 'This is a vodka that tastes like vodka'),
+ ('Cut Spike Rum', 'Rum', 2, '', '', 'This is a rum that tastes like rum'),
+ ('Soldier Valley True American 6', 'Bourbon', 3, '', '', '"This is a handcrafted, small-batch, traditional bourbon. The recipe consists of a blend of locally grown rye, wheat, barley, and corn. This bourbon is distilled slowly to yield a high natural spirit as it comes off the still."'),
+ ('Patriarch Whiskey', 'Whiskey', 3, '16 months', '86%', 'This rough and tumble whiskey is meant to be consumed in a chilled shot or mixed with your favorite cola'),
+ ('Patriarch Blackberry Whiskey', 'Whiskey', 3, '', '80%', 'The first tone you pick up is the subtleness of blackberries followed by the warm finish feeling of the whiskey.'),
+ ('Patriarch Caramel Apple Whiskey', 'Whiskey', 3, '', '80%', '"Made from our prohibition-style Patriarch Whiskey, this whiskey refreshes your palate with the crisp, sweet flavors of real apple and caramel."'),
+ ('Private Stock Rye Whiskey', 'Whiskey', 3, '4 years', '', '"This rye, aged no less than four years, offers a kick of flavor while the corn darkens the color. It is aged to perfection in new American white oak charred barrels."'),
+ ('Signature Rye Whiskey', 'Whiskey', 3, '10 years', '', 'Its pepper and other spice characteristics blend well with subtle hints of vanilla to create a sipping experience you will not soon forget.'),
+ ('Signature Rye Whiskey', 'Whiskey', 3, '12 years', '92%', '"This double-barreled, high-rye recipe was created for a special occasion."');
