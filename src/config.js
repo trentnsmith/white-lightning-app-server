e4c16825bd038d6784e583121b390b07593b744d
Trent Smith <trentnsmith@gmail.com>
2021-05-20 22:38:36 -0500

    ..

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
-    NODE_ENV: process.env.NODE_ENV || 'development',
+    NODE_ENV: process.env.NODE_ENV || 'production',
     DATABASE_URL: process.env.DATABASE_URL || 'postgresql://trent@localhost/white_lightning',
     TEST_DATABASE_URL: process.env.TEST_DATABASE_URL,
   };
